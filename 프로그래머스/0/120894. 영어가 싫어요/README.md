cece3705c6619101e0136102f8c6b343a0b58007
Minhyuk Hong <minster7650@naver.com>
2024-07-09 12:25:14 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/README.md
+++ 프로그래머스/0/120894. 영어가 싫어요/README.md
@@ -1,10 +1,10 @@
 # [level 0] 영어가 싫어요 - 120894 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120894?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120894?language=cpp) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.01 ms
+메모리: 4.2 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 09일 11:43:11
+2024년 07월 09일 12:25:10
 
 ### 문제 설명
 
---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+long long solution(string numbers) {
+    unordered_map<string, int> num_dict = { {"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3}, {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7}, {"eight", 8}, {"nine", 9} };
+    string temp = "";
+    long long answer = 0;
+    for (char c: numbers) {
+        temp += c;
+        if (num_dict.find(temp) != num_dict.end()) {
+            answer = answer * 10 + num_dict[temp];
+            temp = "";
+        }
+    }
+    
+    return answer;
+}
